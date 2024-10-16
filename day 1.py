333032c6d3b338322472c8b5c662f6de944ec535
KrishnaMalode <156291727+KrishnaMalode@users.noreply.github.com>
2024-10-16 20:45:22 +0530

    Add files via upload
    day1

---
+++ day 1.py
@@ -0,0 +1,17 @@
+## taking input and adding conditionals
+
+## Part 1 
+print("Welcome to Talent Battle")
+
+
+#part 2
+vowels = [ 'a','e', 'i', 'o', 'u']
+
+print("Enter an albhabet")
+ip = input()
+
+if ip in vowels:
+    print(f'Input {ip} is a vowel')
+else:
+    print(f'Input {ip} is a consonant')
+
