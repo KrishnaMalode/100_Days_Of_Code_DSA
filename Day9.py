57ed9f26d34f2fd5c96e3a04237432fb2edb6ab4
Krishna Chandrakant Malode <156291727+KrishnaMalode@users.noreply.github.com>
2024-10-19 16:54:55 +0530

    Update Day9.py
    Added Method 2

--- Day9.py
+++ Day9.py
@@ -1,3 +1,20 @@
+## MEthod 1
 ip = int(input("Enter Integer to check no of digits: "))
 ip = str(ip)
 print(len(ip))
+
+## Method 2
+ip = int(input("Enter an Integer to check digits: "))
+
+if(ip == 0):
+  print(f'No of digits in integer {ip} are 0')
+else:
+  count = 0
+  temp = ip
+  while(temp>10):
+    temp = temp/10
+    count += 1
+count+=1
+print(f'No of digits in integer {ip} are {count}')
+
+    
