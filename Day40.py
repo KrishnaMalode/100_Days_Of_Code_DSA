757339a4387510c2e5eb0e984b36454fda235d2b
Krishna Chandrakant Malode <156291727+KrishnaMalode@users.noreply.github.com>
2024-11-25 18:09:35 +0530

    Update Day40.py
    Description
    
    Get a string as input from the user and then get another string which has to be removed from the string.
    
    Get the third input, the new substring which is placed in that substring position.
    
    Finally print the output by replacing the substring with new string.
    
    Input
    
    Enter a string
    
    talentbattle
    
    Enter the substring to be removed :
    
    talent
    
    Enter the new substring :
    
    student
    
    Output
    
    The new string :
    
    studentbattle

--- Day40.py
+++ Day40.py
@@ -1,7 +1,16 @@
 ip = str(input("Enter a string: "))
 find_this = input('Enter substring to be removed: ')
 replace_by =input(f'Enter substring to be replaced by {find_this} => ')
-len_found = len(find_this)
-index = ip.find(find_this)
-ip = ip[:index]+replace_by+ip[index+len_found:]
-print(ip)
+
+if find_this in ip:
+    # method1 Replaces first occurence 
+    len_found = len(find_this)
+    index = ip.find(find_this)
+    ip = ip[:index]+replace_by+ip[index+len_found:]
+    print(ip)
+
+    ## method2 Replaces first occurence
+    ip = ip.replace(find_this,replace_by,1)
+    print(ip)
+else:
+    print("substring not found in string!!")
