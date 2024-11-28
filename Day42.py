0588c3d4cf8faf7c4a064606dada2f3a5e1e9a9b
Krishna Chandrakant Malode <156291727+KrishnaMalode@users.noreply.github.com>
2024-11-28 21:40:32 +0530

    Add files via upload
    Day 42 coding Statement : Write Program to check if two arrays are the same or not
    
    Description
    
    Get two arrays as the input from the user and check whether it is the same or not.
    
    Input
    
    Enter the size of first array :
    
    3
    
    Enter the size of second array :
    
    3
    
    Enter elements of first array :
    
    1 2 3
    
    Enter elements of second array :
    
    1 2 3
    
    Output
    
    Same

---
+++ Day42.py
@@ -0,0 +1,35 @@
+
+lst1 = [int(item) for item in  input('Enter list 1 elements: ').split() ]
+print(lst1)
+lst2 = [int(item) for item in input("Enter list 2  elements: ").split()]
+print(lst2)
+
+if lst1 == lst2:
+    print("Both arrays are same!! ")
+else:
+    print("Both arrays are not same")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
