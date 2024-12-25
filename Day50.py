7d4f505dfd802620821516a425d92c45f7fd1a3f
Krishna Chandrakant Malode <156291727+KrishnaMalode@users.noreply.github.com>
2024-12-25 20:15:30 +0530

    Update Day50.py
    Day 50 coding Statement : Given an integer array of size N. Write Program to find sum of positive square elements in the array.
    
    Sample input 1 :
    
    4 1 2 3 4
    
    Sample output 1 :
    
    30
    
    Explanation :
    
    (1 + 4 + 9 + 16) = 30
    
    Sample input 2 :
    
    4 -1 -2 -3 -4
    
    Sample output 2 :
    
    30
    
    Explanation :
    
    (1 + 4 + 9 + 16) = 30

--- Day50.py
+++ Day50.py
@@ -1,4 +1,7 @@
 some_list = [int(item) for item in input('Please enter your array: ').split()]
+for i in range(0,len(some_list)):
+    some_list[i] = abs(some_list[i])
+
 some_list = list(set(some_list))
 squared_list = []
 for item in some_list:
